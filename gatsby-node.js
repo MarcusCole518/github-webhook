6d72ff64d6dde38d3cb22f1ebbd28c4eb5577c70
“Marcus <mcolelittle@gmail.com>
2022-08-08 22:54:50 -0400

    add trailing slash

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,7 @@ exports.createPages = async ({ graphql, actions }) => {
     defer: true,
   }),
   createRedirect({
-    fromPath: `/page-2`,
+    fromPath: `/page-2/`,
     toPath: `https://www.google.com/`,
     statusCode: 200,
   })
