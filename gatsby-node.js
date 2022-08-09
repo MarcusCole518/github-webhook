fa70fa493b3307616c218e037d336e1bf2f62fba
“Marcus <mcolelittle@gmail.com>
2022-08-08 22:34:31 -0400

    proxy test

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,14 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage, createRedirect } = actions;
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+  createRedirect({
+    fromPath: "/page-2",
+    toPath: "https://twitter.com",
+    statusCode: 200,
   })
 }
