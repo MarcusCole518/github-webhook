126d2645e8efa7e612ac0fc8184f64822614f60f
“Marcus <mcolelittle@gmail.com>
2022-08-08 22:58:45 -0400

    proxy to ccc

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ exports.createPages = async ({ graphql, actions }) => {
   }),
   createRedirect({
     fromPath: `/page-2/`,
-    toPath: `https://www.google.com/`,
+    toPath: `https://collarcitycode.com/`,
     statusCode: 200,
   })
 }
